8a811c69ef34daf914a0889990c6a877ce520c62
chethanahj <chethana0016@gmail.com>
2022-04-09 17:32:30 +0530

    ddddddddddddd

---
+++ project1/src/test/java/project1/second.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class second {
+public static void main(String[] args) {
+		
+	}
+	public void entervaluesTest3() {
+		System.out.println("entered values successfully");
+		
+		
+		
+	}
+
+}
